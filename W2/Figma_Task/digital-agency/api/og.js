690d97f36e8bcfbaddb247636c8886b249fb628a
ShivSPGupta <sspgupta1@yahoo.com>
2025-11-19 21:13:14 +0530

    vercel og

--- W2/Figma_Task/digital-agency/api/og.js
+++ W2/Figma_Task/digital-agency/api/og.js
@@ -1,7 +1,7 @@
 import { ImageResponse } from '@vercel/og';
 
 export const config = {
-  runtime: 'edge',
+  runtime: 'edge'
 };
 
 export default function handler() {
@@ -9,18 +9,18 @@ export default function handler() {
     (
       <div
         style={{
-          display: 'flex',
-          background: "linear-gradient(135deg, #1e3a8a, #3b82f6)",
-          width: '100%',
           height: '100%',
-          color: 'white',
-          alignItems: 'center',
+          width: '100%',
+          background: 'linear-gradient(135deg, #1e3a8a, #3b82f6)',
+          display: 'flex',
           justifyContent: 'center',
-          fontSize: 60,
-          fontWeight: 'bold',
+          alignItems: 'center',
+          color: 'white',
+          fontSize: 64,
+          fontWeight: 700
         }}
       >
-        My React + Vite App
+        My Vite App OG
       </div>
     ),
     { width: 1200, height: 630 }
